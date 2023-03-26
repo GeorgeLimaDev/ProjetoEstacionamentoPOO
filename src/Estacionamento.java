b2b63bda1ddb06346e912d35f6985e58590e4ba6
Amanda Cruz de Araújo <amanda.crz.aj@gmail.com>
2023-03-26 11:46:52 -0300

    Adicionando datas

--- src/Estacionamento.java
+++ src/Estacionamento.java
@@ -1,3 +1,5 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 public class Estacionamento {
@@ -11,7 +13,7 @@ public class Estacionamento {
 		placas = new String[vagas]; // Define o número de vagas do estacionamento.
 		
 		for (int i = 0; i < placas.length; i++) {
-			placas[i] = "livre";
+			placas[i] = "livre"; //Popula o array com vagas "livres";
 		}
 	}
 	
@@ -19,22 +21,34 @@ public class Estacionamento {
 		if (vaga-1 < 0 || vaga-1 > placas.length ) { // Checa se a vaga existe.
 			throw new Exception("Número de vaga inválido.");
 		}
-		else if (!(placas[vaga-1].equals("livre"))) { // Checa se a vaga está livre.
+		else if (!(placas[vaga-1].equals("livre"))) { // Checa se a vaga está ocupada.
 			throw new Exception("Vaga não está livre.");
 		}
 		
 		placas[vaga-1] = placa; // Caso passe nos testes a vaga é ocupada.
+		
+		// Registro da movimentação:
+		LocalDateTime timestamp = LocalDateTime.now();
+		DateTimeFormatter timestampFormat;
+		timestampFormat = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+		System.out.println("Horário da entrada: " + timestamp.format(timestampFormat)); // Print temporário para acompanhar a implementação.
 	}
 	
 	public void sair(int vaga) throws Exception {
 		if (vaga-1 < 0 || vaga-1 > placas.length ) { // Verifica se a vaga existe.
 			throw new Exception("Número de vaga inválido.");
 		}
-		else if (placas[vaga-1].equals("livre")) { // Verifica se a vaga estava ocupada.
+		else if (placas[vaga-1].equals("livre")) { // Verifica se a vaga já estava livre.
 			throw new Exception("Vaga não está ocupada.");
 		}
 		
 		placas[vaga-1] = "livre"; // Caso passe nos testes a vaga é liberada.
+		
+		// Registro da movimentação:
+		LocalDateTime timestamp = LocalDateTime.now();
+		DateTimeFormatter timestampFormat;
+		timestampFormat = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+		System.out.println("Horário da saída: " + timestamp.format(timestampFormat)); // Print temporário para acompanhar a implementação.
 	}
 	
 	public int consultarPlaca(String placa) {
@@ -43,21 +57,28 @@ public class Estacionamento {
 				return i+1; // Retorna o número da vaga.
 			}
 		}
-		return -1;
+		return -1; //A confirmar: deve retornar esse valor padrão ou subir uma exceção?
 	}
 	
 	public void transferir(int vaga1, int vaga2) throws Exception{
-		if (vaga1 == vaga2) {
+		if (vaga1 == vaga2) { // Checa se as vagas de origem e destino são as mesmas.
 			throw new Exception("Tentativa de mover para a mesma posição.");
 		}
 		else if (!(placas[vaga1-1].equals("livre"))) { // Checa se a vaga1 está ocupada.
 				if (placas[vaga2-1].equals("livre")) { // Checa se a vaga2 está livre.
-					placas[vaga2-1] = placas[vaga1-1]; // Realiza a transferência para vaga2
+					placas[vaga2-1] = placas[vaga1-1]; // Realiza a transferência para vaga2.
 					placas[vaga1-1] = "livre"; // Libera a vaga1.
+					
+					// Registro da movimentação:
+					// Dúvida: transferências devem ser registradas como entradas e saídas?
+					LocalDateTime timestamp = LocalDateTime.now();
+					DateTimeFormatter timestampFormat;
+					timestampFormat = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+					System.out.println("Horário da alteração: " + timestamp.format(timestampFormat)); // Print temporário para acompanhar a implementação.
 			}
 		}
 		else {
-			throw new Exception("Pelo menos uma das vagas informada é inválida.");
+			throw new Exception("Pelo menos uma das vagas informadas é inválida.");
 		}
 	}
 	
--- src/Teste2.java
+++ src/Teste2.java
@@ -7,45 +7,43 @@ public class Teste2 {
 	public static void main(String[] args) {
 		Estacionamento estacionamento = null;
 		try {
-			estacionamento = new Estacionamento(10);
-		} catch (Exception e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}	//10 vagas
+			estacionamento = new Estacionamento(10);	//10 vagas
+			new Estacionamento(0);
+		}catch (Exception e) {System.out.println("exceção--->"+e.getMessage());}
 
 		System.out.println("\n-------TESTE EXCEÇÕES LANÇADAS--------");
 		try {
 			estacionamento.entrar("AAA1111",1);
 			estacionamento.entrar("XXX1111",1);
-			System.out.println("*************1--->Nao lançou exceção para: entrar 1"); 
+			System.out.println("*****1--->Nao lançou exceção para: entrar 1"); 
 		}catch (Exception e) {System.out.println("exceção1--->"+e.getMessage());}
 
 		try {
 			estacionamento.sair(2);
-			System.out.println("*************2--->Nao lançou exceção para: sair 2"); 
+			System.out.println("*****2--->Nao lançou exceção para: sair 2"); 
 		}catch (Exception e) {System.out.println("exceção2--->"+e.getMessage());}
 
 		try {
 			estacionamento.consultarPlaca("XXX0000");
-			System.out.println("*************4--->Nao lançou exceção para: consultarPlaca XXX "); 
+			System.out.println("*****4--->Nao lançou exceção para: consultarPlaca XXX "); 
 		}
 		catch (Exception e) {System.out.println("exceção4--->"+e.getMessage());}
 
 		try {
 			estacionamento.transferir(1,1);
-			System.out.println("*************5--->Nao lançou exceção para: transferir 1"); 
+			System.out.println("*****5--->Nao lançou exceção para: transferir 1"); 
 		}
 		catch (Exception e) {System.out.println("exceção5--->"+e.getMessage());}
 
 		try {
 			estacionamento.transferir(2,1);
-			System.out.println("*************6--->Nao lançou exceção para: transferir 2"); 
+			System.out.println("*****6--->Nao lançou exceção para: transferir 2"); 
 		}
 		catch (Exception e) {System.out.println("exceção6--->"+e.getMessage());}
 
 		try {
 			estacionamento.transferir(3,2);
-			System.out.println("*************7--->Nao lançou exceção para: transferir 3 "); 
+			System.out.println("*****7--->Nao lançou exceção para: transferir 3 "); 
 		}
 		catch (Exception e) {System.out.println("exceção7--->"+e.getMessage());}
 
