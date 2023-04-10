803343145f52dc4a6cbfc6875212cc7bdcfccf61
Amanda Cruz de Araújo <amanda.crz.aj@gmail.com>
2023-04-10 10:53:00 -0300

    Implementação da interface - correções e melhorias

--- src/Interface/Interface2.java
+++ src/Interface/Interface2.java
@@ -1,5 +1,7 @@
 import java.awt.EventQueue;
 
+//Grupo: Amanda Cruz de Araújo, George Barbosa de Lima, Ian Ribeiro de Mendonça.
+
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
@@ -13,25 +15,25 @@ import java.awt.Color;
 public class Interface2 {
 
 	private JFrame frame;
-	private JLabel label;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JLabel label_1;
-	private JButton button;
+	private JLabel label_placa;
+	private JTextField Placa;
+	private JTextField Vaga;
+	private JLabel label_vaga;
+	private JButton button_entrar;
 	
 	private Estacionamento estacionamento;
-	private JLabel label_2;
-	private JButton button_1;
-	private JButton button_2;
-	private JLabel label_3;
-	private JTextField textField_2;
-	private JTextField textField_3;
-	private JLabel label_4;
-	private JButton button_3;
-	private JButton button_4;
-	private JButton button_5;
-	private JButton button_6;
-	private JButton button_7;
+	private JLabel label_mensagens;
+	private JButton button_sair;
+	private JButton button_consultar;
+	private JLabel label_vagaOrigem;
+	private JTextField VagaOrigem;
+	private JTextField VagaDestino;
+	private JLabel label_vagaDestino;
+	private JButton button_transferir;
+	private JButton button_listarVagas;
+	private JButton button_listarVagasLivres;
+	private JButton button_gravarDados;
+	private JButton button_lerDados;
 	private JPanel panel;
 	private JPanel panel_1;
 	private JPanel panel_2;
@@ -66,175 +68,179 @@ public class Interface2 {
 		
 		//Criando o objeto estacionamento
 			try {
-				estacionamento = new Estacionamento(10);	//10 vagas
+				estacionamento = new Estacionamento(10);	//estacionamento com 10 vagas
 		 
 			}catch (Exception e) {System.out.println("exceção0--->"+e.getMessage());}
 				
 				
 		frame = new JFrame();
-		frame.setTitle("Estacionamento2");
+		frame.setTitle("Estacionamento");
 		frame.setBounds(100, 100, 608, 400);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		label = new JLabel("Placa:");
-		label.setForeground(new Color(255, 255, 255));
-		label.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		label.setBounds(87, 22, 68, 39);
-		frame.getContentPane().add(label);
+		label_placa = new JLabel("Placa:");
+		label_placa.setForeground(new Color(255, 255, 255));
+		label_placa.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		label_placa.setBounds(87, 22, 68, 39);
+		frame.getContentPane().add(label_placa);
 		
-		textField = new JTextField();
-		textField.setBounds(157, 24, 96, 39);
-		frame.getContentPane().add(textField);
-		textField.setColumns(10);
+		Placa = new JTextField();
+		Placa.setBounds(157, 24, 96, 39);
+		frame.getContentPane().add(Placa);
+		Placa.setColumns(10);
 		
-		textField_1 = new JTextField();
-		textField_1.setColumns(10);
-		textField_1.setBounds(375, 24, 96, 39);
-		frame.getContentPane().add(textField_1);
+		Vaga = new JTextField();
+		Vaga.setColumns(10);
+		Vaga.setBounds(375, 24, 96, 39);
+		frame.getContentPane().add(Vaga);
 		
-		label_1 = new JLabel("Vaga:");
-		label_1.setForeground(new Color(255, 255, 255));
-		label_1.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		label_1.setBounds(312, 22, 68, 39);
-		frame.getContentPane().add(label_1);
+		label_vaga = new JLabel("Vaga:");
+		label_vaga.setForeground(new Color(255, 255, 255));
+		label_vaga.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		label_vaga.setBounds(312, 22, 68, 39);
+		frame.getContentPane().add(label_vaga);
 		
 		//Permite registro de entrada de veículos
 		
-		//Algumas exception não possuem texto, fica estranho 
-		
-		
-		button = new JButton("Entrar Veículo");
-		button.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		button.addActionListener(new ActionListener() {
+		button_entrar = new JButton("Entrar Veículo");
+		button_entrar.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		button_entrar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				try {
-					String placaVaga = textField.getText();
-					int numerovaga = Integer.parseInt(textField_1.getText());
-					estacionamento.entrar(placaVaga,numerovaga);
-					label_2.setText("Entrada registrada");
+					String placa = Placa.getText();
+					int numerovaga = Integer.parseInt(Vaga.getText());
+					estacionamento.entrar(placa,numerovaga);
+					label_mensagens.setText("Entrada registrada");
 				}
-				catch (Exception e1) {label_2.setText(e1.getMessage());}
+				catch (Exception e1) {
+					String mensagemEntrada = e1.getMessage();
+					if (mensagemEntrada.equals("For input string: \"\"")){
+						label_mensagens.setText("É preciso preencher os campus.");}
+					else {label_mensagens.setText(mensagemEntrada);}
+				}
+			
 			}
 							
 		});
-		button.setBounds(50, 72, 147, 45);
-		frame.getContentPane().add(button);
 		
-		label_2 = new JLabel("");
-		label_2.setBounds(87, 314, 419, 39);
-		frame.getContentPane().add(label_2);
+		button_entrar.setBounds(50, 72, 147, 45);
+		frame.getContentPane().add(button_entrar);
+		
+		label_mensagens = new JLabel("");
+		label_mensagens.setBounds(87, 314, 419, 39);
+		frame.getContentPane().add(label_mensagens);
 		
 		//Permite registro de saída de veículos
-		button_1 = new JButton("Sair Veículo");
-		button_1.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		button_1.addActionListener(new ActionListener() {
+		button_sair = new JButton("Sair Veículo");
+		button_sair.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		button_sair.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				try {					
-					int numerovaga = Integer.parseInt(textField_1.getText());
+					int numerovaga = Integer.parseInt(Vaga.getText());
 					estacionamento.sair(numerovaga);
-					label_2.setText("Saída registrada");
+					label_mensagens.setText("Saída registrada");
 				}
-				catch (Exception e1) {label_2.setText(e1.getMessage());}
+				catch (Exception e1) {
+					String mensagemSaida = e1.getMessage();
+					if (mensagemSaida.equals("For input string: \"\"")){
+						label_mensagens.setText("É preciso preencher os campus.");}
+					else {label_mensagens.setText(mensagemSaida);}
 				}
-						
-			
+			}
+					
 		});
-		button_1.setBounds(207, 73, 147, 42);
-		frame.getContentPane().add(button_1);
+		button_sair.setBounds(207, 73, 147, 42);
+		frame.getContentPane().add(button_sair);
 		
 		//Permite consultar em qual vaga está o veículo de determinada placa
 		
-		//Aqui o localPlaca é um inteiro, para colocar no label a solução foi transformar em string. Tem forma melhor?
-		
-		//Precisa de try catch nesse caso? já que não possui exception. E como tratar o -1, deixa assim? Ele aparece, se der "erro"
-		
-		button_2 = new JButton("Consultar Placa");
-		button_2.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		button_2.addActionListener(new ActionListener() {
+		button_consultar = new JButton("Consultar Placa");
+		button_consultar.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		button_consultar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				try {
-					String placaVaga = textField.getText();
-					int localPlaca = estacionamento.consultarPlaca(placaVaga);
-					String localPlaca2 = Integer.toString(localPlaca);
-					label_2.setText("O veículo de placa " + placaVaga + " está na vaga " + localPlaca2);
-					}
-				catch (Exception e1) {label_2.setText("localPlaca");}
+				String placa = Placa.getText();
+				String vagaPlaca = Integer.toString(estacionamento.consultarPlaca(placa));
+				
+				if(vagaPlaca.equals("-1")) {
+					label_mensagens.setText("A placa não está registrada.");}
+				
+				else {
+					label_mensagens.setText("O veículo de placa " + placa + " está na vaga " + vagaPlaca);}
 			}
 				
 			
 		});
-		button_2.setBounds(362, 71, 144, 42);
-		frame.getContentPane().add(button_2);
+		button_consultar.setBounds(362, 71, 144, 42);
+		frame.getContentPane().add(button_consultar);
 		
-		label_3 = new JLabel("Vaga origem:");
-		label_3.setForeground(new Color(255, 255, 255));
-		label_3.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		label_3.setBounds(60, 127, 107, 51);
-		frame.getContentPane().add(label_3);
+		label_vagaOrigem = new JLabel("Vaga origem:");
+		label_vagaOrigem.setForeground(new Color(255, 255, 255));
+		label_vagaOrigem.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		label_vagaOrigem.setBounds(60, 127, 107, 51);
+		frame.getContentPane().add(label_vagaOrigem);
 		
-		textField_2 = new JTextField();
-		textField_2.setColumns(10);
-		textField_2.setBounds(167, 135, 96, 39);
-		frame.getContentPane().add(textField_2);
+		VagaOrigem = new JTextField();
+		VagaOrigem.setColumns(10);
+		VagaOrigem.setBounds(167, 135, 96, 39);
+		frame.getContentPane().add(VagaOrigem);
 		
-		textField_3 = new JTextField();
-		textField_3.setColumns(10);
-		textField_3.setBounds(385, 135, 96, 39);
-		frame.getContentPane().add(textField_3);
+		VagaDestino = new JTextField();
+		VagaDestino.setColumns(10);
+		VagaDestino.setBounds(385, 135, 96, 39);
+		frame.getContentPane().add(VagaDestino);
 		
-		label_4 = new JLabel("Vaga destino:");
-		label_4.setForeground(new Color(255, 255, 255));
-		label_4.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		label_4.setBounds(283, 127, 107, 51);
-		frame.getContentPane().add(label_4);
+		label_vagaDestino = new JLabel("Vaga destino:");
+		label_vagaDestino.setForeground(new Color(255, 255, 255));
+		label_vagaDestino.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		label_vagaDestino.setBounds(283, 127, 107, 51);
+		frame.getContentPane().add(label_vagaDestino);
 		
 		//Permite transferir o veículo de vaga
-		button_3 = new JButton("Transferir Veículo");
-		button_3.addActionListener(new ActionListener() {
+		button_transferir = new JButton("Transferir Veículo");
+		button_transferir.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				try {					
-					int vagaOrigem = Integer.parseInt(textField_2.getText());
-					int vagaDestino = Integer.parseInt(textField_3.getText());
+					int vagaOrigem = Integer.parseInt(VagaOrigem.getText());
+					int vagaDestino = Integer.parseInt(VagaDestino.getText());
 					
 					estacionamento.transferir(vagaOrigem,vagaDestino);
-					label_2.setText("Transferência registrada");
+					label_mensagens.setText("Transferência registrada");
 				}
-				catch (Exception e1) {label_2.setText(e1.getMessage());}
 				
+				catch (Exception e1) {
+					String mensagemTransf = e1.getMessage();
+					if (mensagemTransf.equals("For input string: \"\"")){
+						label_mensagens.setText("É preciso preencher os campus.");}
+					else {label_mensagens.setText(mensagemTransf);}
+				}
 			}
 		});
-		button_3.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		button_3.setBounds(217, 184, 167, 45);
-		frame.getContentPane().add(button_3);
+		button_transferir.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		button_transferir.setBounds(217, 184, 167, 45);
+		frame.getContentPane().add(button_transferir);
 		
 		//Permite listar as vagas, informa se está livre e, se não, informa a placa do carro da vaga
-		//Não tem expetion, removemos o try/catch?
-		button_4 = new JButton("Listar Vagas");
-		button_4.addActionListener(new ActionListener() {
+		
+		button_listarVagas = new JButton("Listar Vagas");
+		button_listarVagas.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				try {						
 					String sitVagas = "";
 																				
 					for(String s : estacionamento.listarGeral()) {
-						sitVagas += s + " ";
-					}
-					label_2.setText("Vagas: " + sitVagas);
-				}
-				catch (Exception e1) {label_2.setText(e1.getMessage());}
+						sitVagas += s + " ";}
+					label_mensagens.setText("Vagas: " + sitVagas);
 				
 			}
 		});
-		button_4.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		button_4.setBounds(37, 259, 119, 45);
-		frame.getContentPane().add(button_4);
+		button_listarVagas.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		button_listarVagas.setBounds(37, 259, 119, 45);
+		frame.getContentPane().add(button_listarVagas);
 		
-		
-		
-		
+						
 		//Permite listar as vagas livres
-		button_5 = new JButton("Listar Vagas Livres");
-		button_5.addActionListener(new ActionListener() {
+		button_listarVagasLivres = new JButton("Listar Vagas Livres");
+		button_listarVagasLivres.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				try {						
 					String vagasLivres = "";
@@ -242,50 +248,50 @@ public class Interface2 {
 					for(int i : estacionamento.listarLivres()) {
 						vagasLivres += i + " ";
 					}
-					label_2.setText("Vagas livres: " + vagasLivres);
+					label_mensagens.setText("Vagas livres: " + vagasLivres);
 				}
-				catch (Exception e1) {label_2.setText(e1.getMessage());}
+				catch (Exception e1) {label_mensagens.setText(e1.getMessage());}
 			}
 					
 		});
-		button_5.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		button_5.setBounds(157, 259, 163, 45);
-		frame.getContentPane().add(button_5);
+		button_listarVagasLivres.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		button_listarVagasLivres.setBounds(157, 259, 163, 45);
+		frame.getContentPane().add(button_listarVagasLivres);
 		
 		
-		//Não sei se está funcionando, não testei bem esse
-		button_6 = new JButton("Gravar Dados");
-		button_6.addActionListener(new ActionListener() {
+		//Permite gravar os dados
+		button_gravarDados = new JButton("Gravar Dados");
+		button_gravarDados.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				try {					
 					estacionamento.gravarDados();
-					label_2.setText("Dados gravados.");
+					label_mensagens.setText("Dados gravados.");
 				}
-				catch (Exception e1) {label_2.setText(e1.getMessage());}
+				catch (Exception e1) {label_mensagens.setText(e1.getMessage());}
 				
 				
 			}
 		});
-		button_6.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		button_6.setBounds(321, 259, 119, 45);
-		frame.getContentPane().add(button_6);
+		button_gravarDados.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		button_gravarDados.setBounds(321, 259, 119, 45);
+		frame.getContentPane().add(button_gravarDados);
 		
 		
-		//Não sei se está funcionando, não testei bem esse!
-		button_7 = new JButton("Ler Dados");
-		button_7.addActionListener(new ActionListener() {
+		//Permite listar os dados
+		button_lerDados = new JButton("Ler Dados");
+		button_lerDados.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				try {					
 					estacionamento.lerDados();
-					label_2.setText("Dados lidos?????");
+					label_mensagens.setText("Dados lidos?????");
 				}
-				catch (Exception e1) {label_2.setText(e1.getMessage());}
+				catch (Exception e1) {label_mensagens.setText(e1.getMessage());}
 				
 			}
 		});
-		button_7.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		button_7.setBounds(442, 259, 107, 45);
-		frame.getContentPane().add(button_7);
+		button_lerDados.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		button_lerDados.setBounds(442, 259, 107, 45);
+		frame.getContentPane().add(button_lerDados);
 		
 		panel = new JPanel();
 		panel.setBackground(new Color(0, 0, 64));
